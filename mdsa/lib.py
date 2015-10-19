9e5a52530b5f019ea888024de5c040e0ba914968
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-10-19 16:09:00 -0700

    ENH: Add base-package and gitignore
    Fixes #6

---
+++ mdsa/lib.py
@@ -0,0 +1,28 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2015--, mdsa development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+from __future__ import absolute_import, division, print_function
+
+def return_forty_two(strict=True):
+    """Function that returns the integer 42
+
+    Parameters
+    ----------
+    strict : bool, optional
+        Whether we'll return a strict value of 42 or not. Defaults to False.
+
+    Returns
+    -------
+    int
+        The value 42
+
+    Notes
+    -----
+    The return value of this function is also known as "The answer to life, the
+    universe and everything.".
+    """
+    return 42
