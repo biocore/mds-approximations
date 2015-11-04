2a4437e1a86395d2b92f3861fa01e20ea9bf1297
Jamie Morton <jamietmorton@gmail.com>
2015-11-04 14:53:50 -0800

    DEV: Adding scmds, nystrom and corresponding unittests

---
+++ mdsa/nystrom.py
@@ -0,0 +1,23 @@
+from mdsa.approximate_mds import principal_coordinates_analysis
+
+def nystrom(dm, k=10):
+    """ Performs nystrom approximation of pca
+
+    Parameters
+    ----------
+    dm : np.array
+        Distance matrix
+    k : int
+        Number of dimensions (i.e. eigenvectors)
+        to return
+
+    Returns
+    -------
+    coords : np.array
+        List of eigenvectors
+    eigvals : np.array
+        Eigenvalues
+    pcnts : np.array
+        Percentage of variation explained
+    """
+    return principal_coordinates_analysis(dm, 'nystrom', k)
