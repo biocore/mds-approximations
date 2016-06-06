330310b1c2cb5d047b686a968910421d3af53381
Hannes Holste <hannes.holste@gmail.com>
2016-06-06 16:04:38 -0700

    Remove unnecessary imports and add console output

--- mdsapprox.py
+++ mdsapprox.py
@@ -1,5 +1,4 @@
 import unittest
-from collections import defaultdict
 
 import click
 
@@ -15,12 +14,12 @@ def main():
 @click.option('--directory', type=click.Path(), default='.',
               help='Relative path of directory containing tests')
 def test(verbosity, directory):
+    click.echo('Running tests via unittest from directory (output verbosity level: %s): %s' % (verbosity, directory))
     verbosity_codes = dict(quiet=0, default=1, verbose=2)
 
     # Run all tests
     testsuite = unittest.TestLoader().discover(directory)
     unittest.TextTestRunner(verbosity=verbosity_codes[verbosity]).run(testsuite)
-
 
 main.add_command(test)
 
