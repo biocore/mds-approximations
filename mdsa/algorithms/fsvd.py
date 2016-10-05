4fc7aa5b8a1506f8a40a748fa0cc9e733442c0dd
Hannes Holste <hannes.holste@gmail.com>
2016-10-04 17:43:44 -0700

    Improve documentation and introduce parameters

--- mdsa/algorithms/fsvd.py
+++ mdsa/algorithms/fsvd.py
@@ -8,31 +8,45 @@ class FSVD(Algorithm):
     def __init__(self):
         super(FSVD, self).__init__(algorithm_name='fsvd')
 
-    def run(self, distance_matrix, num_dimensions_out=10):
+    def run(self, distance_matrix, num_dimensions_out=10,
+            use_power_method=False, num_levels=1):
         """
-        Takes an input matrix and returns eigenvalues and eigenvectors
-        based on algorithm described in 'An Algorithm for the Principal
-        Component analysis
-        of Large Data Sets' by N. Halko, P.G. Martinsson, Y. Shkolnisky,
-        and M. Tygert
-        Matlab code: http://stats.stackexchange.com/questions/2806/best
-        -pca-algorithm-for-huge-number-of-features
-        Paper: https://arxiv.org/abs/1007.5510
+        Performs a singular value decomposition.
 
-        distance_matrix: distance_matrix
-        num_dimensions_out: dimensions
-        Constants:
-        num_levels: is the number of levels of the Krylov method to use, for
-        most applications, num_levels=1 or num_levels=2 is sufficient
-        use_power_method: changes the power of the spectral norm (minimizing
-        the error). See p11/eq8.1 DOI = {10.1137/100804139}
+        Parameters
+        ----------
+        distance_matrix: np.array
+            Numpy matrix representing the distance matrix for which the
+            eigenvectors and eigenvalues shall be computed
+        num_dimensions_out: int
+            Number of dimensions to keep. Must be lower than or equal to the
+            rank of the given distance_matrix.
+        num_levels: int
+            Number of levels of the Krylov method to use (see paper).
+            For most applications, num_levels=1 or num_levels=2 is sufficient.
+        use_power_method: bool
+            Changes the power of the spectral norm, thus minimizing
+            the error). See paper p11/eq8.1 DOI = {10.1137/100804139}
+
+        Returns
+        -------
+        np.array
+            Array of eigenvectors, each with num_dimensions_out length.
+        np.array
+            Array of eigenvalues, a total number of num_dimensions_out.
+
+        Notes
+        -----
+        The algorithm is based on 'An Algorithm for the Principal
+        Component analysis of Large Data Sets'
+        by N. Halko, P.G. Martinsson, Y. Shkolnisky, and M. Tygert.
+        Original Paper: https://arxiv.org/abs/1007.5510
+
+        Ported from reference MATLAB implementation: https://goo.gl/JkcxQ2
         """
         super(FSVD, self).run(distance_matrix, num_dimensions_out)
 
         m, n = distance_matrix.shape
-
-        use_power_method = False
-        num_levels = 1
 
         # Take (conjugate) transpose if necessary, because it makes H smaller,
         # leading to faster computations
