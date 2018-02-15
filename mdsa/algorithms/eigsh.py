0e56342eaac21878c1080270fbc69d7986dd184f
Hannes Holste <HannesHolste@users.noreply.github.com>
2018-02-15 04:07:50 -0800

    Handle edge case of eigsh not converging (#40)
    * Handle edge case of eigsh not converging
    
    * Impute eigenvecs and vals with NaNs if no convergence
    
    * Unit test for eigsh no convergence case
    
    * Set eigsh to shift-inter mode to avoid no convergence exception
    
    * pep8 style fixes

--- mdsa/algorithms/eigsh.py
+++ mdsa/algorithms/eigsh.py
@@ -1,4 +1,7 @@
-from scipy.sparse.linalg import eigsh
+from warnings import warn
+
+import numpy as np
+from scipy.sparse.linalg import eigsh, ArpackNoConvergence
 
 from mdsa.algorithm import Algorithm
 
@@ -14,7 +17,57 @@ class Eigsh(Algorithm):
     def run(self, distance_matrix, num_dimensions_out=10):
         super(Eigsh, self).run(distance_matrix, num_dimensions_out)
 
-        eigenvalues, eigenvectors = eigsh(distance_matrix,
-                                          k=num_dimensions_out)
+        distance_matrix = np.array(distance_matrix)
 
+        try:
+            # Perform eigendecomposition in eigsh shift-inter mode ('LM' -
+            # largest magnitude eigenvalue finder near sigma=0)
+            # to avoid no convergence that sometimes occurs for large randomly
+            # generated distance matrices using the 'SM' finder mode
+            # (smallest magnitude eigenvalue finder)
+            eigenvalues, eigenvectors = eigsh(distance_matrix,
+                                              k=num_dimensions_out,
+                                              return_eigenvectors=True,
+                                              sigma=0,
+                                              which='LM')
+        except ArpackNoConvergence as e:
+            warn('eigsh unable to converge, only returning {} currently '
+                 'converged eigenvectors and {} eigenvalues:\n {}\n\n'
+                 .format(len(e.eigenvectors), len(e.eigenvalues), str(e)),
+                 RuntimeWarning)
+            # since there are missing eigenvalues and vectors when ARPACK
+            # does not converge, we need to fill in the missing values with
+            # NaN so these results can be serialized in a later processing step
+            # without throwing an exception
+            eigenvectors = e.eigenvectors
+            eigenvalues = e.eigenvalues
+            eigenvectors, eigenvalues = \
+                self._impute_eigendecomposition_results(
+                    eigenvectors, eigenvalues, distance_matrix.shape[0])
+
+        return eigenvectors, eigenvalues
+
+    @staticmethod
+    def _impute_eigendecomposition_results(eigenvectors, eigenvalues,
+                                           expected_num_results):
+        """
+            Fills in expected_num_results number of NaNs in eigenvalues array;
+            and generates expected_num_results number of arrays of NaNs to
+            append to eigenvectors array.
+
+        """
+        eigenvalues = np.array(eigenvalues)
+        eigenvectors = np.array(eigenvectors)
+
+        # impute missing eigenvectors, composed of NaN entries
+        nan_eigenvecs = np.array([[np.nan] * eigenvectors.shape[1]] *
+                                 (expected_num_results -
+                                  len(eigenvectors)))
+        eigenvectors = np.concatenate((eigenvectors, nan_eigenvecs),
+                                      axis=0)
+
+        # impute missing eigenvalues as NaNs
+        nan_eigenvals = [[np.nan] * (expected_num_results -
+                                     len(eigenvalues))]
+        eigenvalues = np.append(eigenvalues, nan_eigenvals)
         return eigenvectors, eigenvalues
--- mdsa/tests/test_eigsh.py
+++ mdsa/tests/test_eigsh.py
@@ -22,6 +22,26 @@ class TestEigsh(unittest.TestCase):
         self.assertEqual(eigenvectors.shape, (100, DIMENSIONS_OUT))
         self.assertEqual(eigenvalues.shape, (DIMENSIONS_OUT,))
 
+    def test_eigsh_impute(self):
+        dm = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]])
+        dm_samples = dm.shape[0]
+
+        evec = np.array([[1, 2, 3], [1, 2, 3]])
+        evals = np.array([1, 2, 3])
+
+        evec, evals = self.eigsh._impute_eigendecomposition_results(evec,
+                                                                    evals,
+                                                                    dm_samples)
+
+        expected_evec = np.array([[1., 2., 3.], [1., 2., 3.],
+                                  [np.nan, np.nan, np.nan],
+                                  [np.nan, np.nan, np.nan]])
+
+        expected_evals = np.array([1., 2., 3., np.nan])
+
+        self.assertTrue(evec.tolist(), expected_evec.tolist())
+        self.assertTrue(evals.tolist(), expected_evals.tolist())
+
 
 if __name__ == '__main__':
     unittest.main()
